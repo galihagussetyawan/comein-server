317bae6acfc192add43d30f8158f4f8273ff94e5
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-12 10:22:24 +0700

    refactor: temporarily enable all origin cors for development mode

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  };
   const app = await NestFactory.create(AppModule);
+  app.enableCors(options);
   await app.listen(8080);
 }
 bootstrap();
